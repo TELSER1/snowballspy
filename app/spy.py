9e1954eda7c7899c63721d7849bc7d0ddd409cbc
T Elser <telser8@gmail.com>
2020-09-07 11:41:02 -0700

    removing PIR motion detection, adding handrolled with picamera

--- app/spy.py
+++ app/spy.py
@@ -18,7 +18,15 @@ def video_to_gcs(filename_):
     logging.info("wrote file {0} to GCS".format(filename_))
     os.remove("/home/pi/videos/" + filename_)
     logging.info("removed file {0} from local system".format(filename_))
-        
+
+def take_photo():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (320, 240)
+        camera.framerate = 24
+        time.sleep(2)
+        output = np.empty((240, 320, 3), dtype=np.uint8)
+        camera.capture(output, 'rgb')
+        return output
 
 if not os.path.exists('/home/pi/snowball_log'):
     os.makedirs('/home/pi/snowball_log')
@@ -32,8 +40,8 @@ logging.basicConfig(filename=logfile, level=logging.DEBUG,
     format='%(asctime)s %(message)s',
     datefmt='%Y-%m-%d, %H:%M:%S,')
 
-pir = MotionSensor(17)
 duration = int(config['spyparams']['duration'])
+threshold = int(config['spyparams']['threshold'])
 
 print('Starting')
 logging.info('Starting')
@@ -42,40 +50,36 @@ logging.info('Starting')
 print('Waiting for sensor to settle')
 time.sleep(10)
 print('Ready')
-
+previous_image = take_photo()
 while True:
-    pir.wait_for_motion()
-    logging.info('Motion detected')
-    print('Motion detected')
-    while pir.motion_detected:
-        print('Beginning capture')
-        ts = '{:%Y%m%d-%H%M%S}'.format(datetime.now())
-        logging.info('Beginning capture: '+ str(ts)+'.h264')
-        with picamera.PiCamera() as cam:
-            cam.resolution=(1024,768)
-            cam.annotate_background = picamera.Color('black')
+    current_image = take_photo()
+    if np.sum(np.abs(current_image - previous_image)) > threshold:
+            logging.info('Motion detected')
+            print('Motion detected')
+            time_end = time.time() + 60*duration
+            ts = '{:%Y%m%d-%H%M%S}'.format(datetime.now())
 
-            cam.start_recording('/home/pi/video.h264')
-            start = datetime.now()
-            while (datetime.now() - start).seconds < duration:
-                cam.annotate_text = datetime.now().strftime('%d-%m-%y %H:%M:%S')
-                cam.wait_recording(0.2)
-            cam.stop_recording()
-        time.sleep(1)
-        print('Stopped recording')
-        timestamp = datetime.now().strftime('%d-%m-%y_%H-%M-%S')
-        input_video = "/home/pi/video.h264"
+            with picamera.PiCamera() as cam:
+                cam.resolution=(1024,768)
+                cam.annotate_background = picamera.Color('black')
+                cam.start_recording('/home/pi/video.h264')
+                while time.time() < t_end:
+                    cam.annotate_text = datetime.now().strftime('%d-%m-%y %H:%M:%S')
+                    cam.wait_recording(0.2)
+                cam.stop_recording()
+            timestamp = datetime.now().strftime('%d-%m-%y_%H-%M-%S')
+            input_video = "/home/pi/video.h264"
+            logging.info('Attempting to save video')
+            print('Attempting to save video')
 
-        logging.info('Attempting to save video')
-        print('Attempting to save video')
+            logging.info('Saving to /home/pi/videos/')
+            print('Saving to /home/pi/videos/')
+            output_video = "/home/pi/videos/{}.mp4".format(timestamp)
+            call(["MP4Box", "-add", input_video, output_video])
+            video_to_gcs("{}.mp4".format(timestamp))
 
-        logging.info('Saving to /home/pi/videos/')
-        print('Saving to /home/pi/videos/')
-        output_video = "/home/pi/videos/{}.mp4".format(timestamp)
-        call(["MP4Box", "-add", input_video, output_video])
-        video_to_gcs("{}.mp4".format(timestamp))
-
-        print('Motion ended - sleeping for 10 secs')
-        logging.info('Motion Ended')
-        time.sleep(10)
-        
+            print('Motion ended - sleeping for 10 secs')
+            logging.info('Motion Ended')
+            time.sleep(120)
+    previous_image = current_image
+    time.sleep(10)
